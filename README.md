16475e3b5df5c702932ea14a417ca80341e37d90
Szymon Kargol <sz.kargol@gmail.com>
2024-07-19 22:44:15 +0200

    Project setup.

---
+++ README.md
@@ -0,0 +1,15 @@
+# vue-rick-and-morty
+
+Vue 3 App consuming Rick and Morty API.
+
+## Project Setup
+
+```sh
+npm i
+```
+
+### Run for Development
+
+```sh
+npm run dev
+```
---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>Rick and Morty</h1>
+  <router-view></router-view>
+</template>
+
+<style lang="scss">
+#app {
+  font-family: Arial, sans-serif;
+  max-width: 1200px;
+  width: 100%;
+  margin: 0 auto;
+  padding: 20px;
+}
+</style>
